599d6be5bd0056b161304ae7fbf746b118bf91bd
alexandru-posirca <axelltech.dev@gmail.com>
2024-06-04 00:07:14 +0300

    Header component - dynamic elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Content from "./components/Content"
+import Header from "./components/Header"
+
 function App() {
 
   return (
     <>
-      <div>
-        <h2 className='text-3xl'>First commit</h2>
-      </div>
+      <Header/>
+      <Content/>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "black-main": "#000000",
+      "white-main": "#FAFAF9",
+      green: "#38af48",
+      gray: {
+        200: "#b9b9ba",
+        500: "#171619",
+      },
+    },
     extend: {},
   },
   plugins: [],
